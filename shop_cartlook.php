0934458c9448cfef92b2d184af25ad72dee65b4d
Tomonori Ueno <t.uepon@gmail.com>
2017-07-28 12:07:38 +0900

    200Pのように編集

--- shop_cartlook.php
+++ shop_cartlook.php
@@ -40,6 +40,29 @@ $password='123456';
 $dbh=new PDO($dsn,$user,$password);
 $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
+foreach($cart as $key => $val);
+{
+    $sql='SELECT code,name,price,gazou FROM mst_product WHERE code=?';
+    $stmt=$dbh->prepare($sql);
+    $data[0]=$val;
+    $stmt->execute($data);
+
+    $rec=$stmt->fetch(PDO::FETCH_ASSOC);
+
+    $pro_name[]=$rec['name'];
+    $pro_price[]=$rec['price'];
+    if($rec['gazou'])=='')
+    {
+        $pro_gazou[]='';
+    }
+    else
+    {
+        $pro_gazou[]='<img src="../product/gazou/'.$rec['gazou'].'">';
+    }
+
+}
+$dbh=null;
+
 }
 catch(Exception $e)
 {
