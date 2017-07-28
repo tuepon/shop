285821c28cb911f44fd946db682cee120aff6cfd
Tomonori Ueno <t.uepon@gmail.com>
2017-07-28 20:15:49 +0900

    P.223 同じ商品をカートに入れさせない仕様

--- shop_cartin.php
+++ shop_cartin.php
@@ -37,6 +37,12 @@ if(isset($_SESSION['cart'])==true)
 {
     $cart=$_SESSION['cart'];
     $kazu=$_SESSION['kazu'];
+    if(in_array($pro_code,$cart)==true)
+    {
+        print 'その商品はすでにカートの中に入っています。<br>';
+        print '<a href="shop_list.php"> 商品一覧に戻る </a>';
+        exit();
+    }
 }
 $cart[]=$pro_code;
 $kazu[]=1;
