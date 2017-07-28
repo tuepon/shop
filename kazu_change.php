9560d230b509619f04cd74c9408864ffd99b69f0
Tomonori Ueno <t.uepon@gmail.com>
2017-07-28 20:41:33 +0900

    P.226 数量のチェック

--- kazu_change.php
+++ kazu_change.php
@@ -9,12 +9,24 @@
     $max=$post['max'];
     for($i=0;$i<$max;$i++)
     {
+        if(preg_match("/\A[0-9]+\z/", $post['kazu'.$i])==0) //半角数字以外入れさせない
+        {
+            print '数量に誤りがあります。';
+            print '<a href="shop_cartlook.php"> カートに戻る </a>';
+            exit();
+        }
+        if($post['kazu'.$i]<1 || 10<$post['kazu'.$i])
+        {
+            print '数量は必ず１個以上、１０個までです。';
+            print '<a href="shop_cartlook.php"> カートに戻る </a>';
+            exit();
+        }
         $kazu[]=$post['kazu'.$i];
     }
 
     $cart=$_SESSION['cart'];
 
-    for($i=$max;0<=$i;i--)
+    for($i=$max;0<=$i;$i--)
     {
         if(isset($_POST['sakujyo'.$i])==true)
         {
