4c7a2a4e3aae7040f56074be2404044f07e906e6
Tomonori Ueno <t.uepon@gmail.com>
2017-07-29 21:59:45 +0900

    P249 完成版

--- shop_form_done.php
+++ shop_form_done.php
@@ -34,6 +34,70 @@ print $postal1.'-'.$postal2.'<br>';
 print $address.'<br>';
 print $tel.'<br>';
 
+$honbun='';
+$honbun.=$onamae."様\n\nこのたびはご注文ありがとうございました。\n";
+$honbun.="\n";
+$honbun.="ご注文商品\n";
+$honbun.="----------\n";
+
+$cart=$_SESSION['cart'];
+$kazu=$_SESSION['kazu'];
+$max=count($cart);
+
+$dsn='mysql:dbname=shop;host=localhost;charset=utf8'; //DB接続コピペ
+$user='root';
+$password='123456';
+$dbh=new PDO($dsn,$user,$password);
+$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+for($i=0;$i<$max;$i++)
+{
+	$sql='SELECT name,price FROM mst_product WHERE code=?';
+    $stmt=$dbh->prepare($sql);
+    $data[0]=$val;
+    $stmt->execute($data);
+
+    $rec=$stmt->fetch(PDO::FETCH_ASSOC);
+
+	$name=$rec['name'];
+	$price=$rec['price'];
+	$suryo=$kazu[$i];
+	$shokei=$price*$suryo;
+}	
+
+$dbh=null;
+
+$honbun.="送料は無料です。\n";
+$honbun.="-----------\n";
+$honbun.="\n";
+$honbun.="代金は以下の口座にお振込みください。\n";
+$honbun.="ろくまる銀行　やさい支店　普通口座　1234567\n";
+$honbun.="入金確認が取れ次第、梱包、発送させていただきます。\n";
+$honbun.="\n";
+$honbun.="□□□□□□□□□□□□□□□□□□□□□□□□□□\n";
+$honbun.="　～安心野菜のろくまる農園～\n";
+$honbun.="\n";
+$honbun.="〇〇県六丸郡六丸村　123-4\n";
+$honbun.="電話　090-6060-xxxx\n";
+$honbun.="メール　info@example.com\n";
+$honbun.="□□□□□□□□□□□□□□□□□□□□□□□□□□\n";
+//print '<br>';
+//print nl2br($honbun);
+
+$title='ご注文ありがとうございます。'; //顧客にメール送信
+$header='From: info@example.com';
+$honbun=html_entity_decode($honbun,ENT_QUOTES,'UTF-8');
+mb_language('Japanese');
+mb_internal_encoding('UTF-8');
+mb_send_mail($email,$title,$honbun,$header);
+
+$title='お客様からご注文がありました。'; //店にメール送信
+$header='From:'.$email;
+$honbun=html_entity_decode($honbun,ENT_QUOTES,'UTF-8');
+mb_language('Japanese');
+mb_internal_encoding('UTF-8');
+mb_send_mail('info@example.com',$title,$honbun,$header);
+
 }
 catch(Exception $e)
 {
