832480bca3e46ba7da10988499b98d93a0e0fb65
Tomonori Ueno <t.uepon@gmail.com>
2017-07-29 18:26:32 +0900

    P239　入力不備なら戻るボタンだけ表示するよう変更

--- shop_form_check.php
+++ shop_form_check.php
@@ -18,9 +18,12 @@ $postal2=$post['postal2'];
 $address=$post['address'];
 $tel=$post['tel'];
 
+$okflg=true; //フラグ制御
+
 if($onamae=='')
 {
 	print 'お名前が入力されていません。<br><br>';
+	$okflg=false;
 }
 else
 {
@@ -32,7 +35,8 @@ else
 if(preg_match('/\A[\w\-\.]+\@[\w\-\.]+\.([a-z]+)\z/',$email)==0)
 {
 	print 'メールアドレスを正確に入力してください。<br><br>';
-}
+	$okflg=false;
+	}
 else
 {
 	print ' メールアドレス <br>';
@@ -43,7 +47,8 @@ else
 if(preg_match('/\A[0-9]+\z/',$postal1)==0)
 {
 	print '郵便番号は半角数字で入力してください。<br><br>';
-}
+	$okflg=false;
+	}
 else
 {
 	print ' 郵便番号 <br>';
@@ -56,12 +61,14 @@ else
 if(preg_match('/\A[0-9]+\z/',$postal2)==0)
 {
 	print '郵便番号は半角数字で入力してください。<br><br>';
+	$okflg=false;
 }
 
 if($address=='')
 {
 	print '住所が入力されていません。<br><br>';
-}
+	$okflg=false;
+	}
 else
 {
 	print ' 住所 <br>';
@@ -72,7 +79,8 @@ else
 if(preg_match('/\A\d{2,5}-?\d{2,5}-?\d{4,5}\z/',$tel)==0)
 {
 	print '電話番号を正確に入力してください。<br><br>';
-}
+	$okflg=false;
+	}
 else
 {
 	print ' 電話番号 <br>';
@@ -80,16 +88,25 @@ else
 	print '<br><br>';
 }
 
-print '<form method="post" action="shop_form-done.php">';
-print '<input type="hidden" name="onamae" value="'.$onamae.'">';
-print '<input type="hidden" name="email" value="'.$email.'">';
-print '<input type="hidden" name="postal1" value="'.$postal1.'">';
-print '<input type="hidden" name="postal2" value="'.$postal2.'">';
-print '<input type="hidden" name="address" value="'.$address.'">';
-print '<input type="hidden" name="tel" value="'.$tel.'">';
-print '<input type="button" onclick="history.back()" value=" 戻る ">';
-print '<input type="submit" value="OK"><br>';
-print '</form>';
+if($okflg==true)
+{
+	print '<form method="post" action="shop_form-done.php">';
+	print '<input type="hidden" name="onamae" value="'.$onamae.'">';
+	print '<input type="hidden" name="email" value="'.$email.'">';
+	print '<input type="hidden" name="postal1" value="'.$postal1.'">';
+	print '<input type="hidden" name="postal2" value="'.$postal2.'">';
+	print '<input type="hidden" name="address" value="'.$address.'">';
+	print '<input type="hidden" name="tel" value="'.$tel.'">';
+	print '<input type="button" onclick="history.back()" value=" 戻る ">';
+	print '<input type="submit" value="OK"><br>';
+	print '</form>';
+}
+else
+{
+	print '<form>';
+	print '<input type="button" onclick="history.back()" value=" 戻る ">';
+	print '</form>';
+}
 
 ?>
 </body>
