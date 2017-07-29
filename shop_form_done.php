6c6ca5fb9920788ce5f3a2598ad075f9da89eb44
Tomonori Ueno <t.uepon@gmail.com>
2017-07-29 20:10:30 +0900

    P242-P243 注文受付の文言表示

---
+++ shop_form_done.php
@@ -0,0 +1,46 @@
+<?php
+	session_start();
+	session_regenerate_id(true);
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>注文登録</title>
+  </head>
+<body>
+
+<?php
+
+try
+{
+require_once('../common/common.php');
+
+$post=sanitize($_POST);
+
+$onamae=$post['onamae'];
+$email=$post['email'];
+$postal1=$post['postal1'];
+$postal2=$post['postal2'];
+$address=$post['address'];
+$tel=$post['tel'];
+
+print $onamae.'様 <br>';
+print 'ご注文ありがとうございました。<br>';
+print $email.'にメールを送りましたのでご確認ください。<br>';
+print '商品は以下の住所に発送させていただきます。<br>';
+print $postal1.'-'.$postal2.'<br>';
+print $address.'<br>';
+print $tel.'<br>';
+
+}
+catch(Exception $e)
+{
+	print 'ただいま障害により大変ご迷惑をお掛けしております。';
+	exit();
+}
+
+?>
+</body>
+</html>
