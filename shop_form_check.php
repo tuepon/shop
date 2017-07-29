3cd6a00dc1f35e9878f5142280d00158d829f018
Tomonori Ueno <t.uepon@gmail.com>
2017-07-29 21:55:51 +0900

    飛び先を修正した

--- shop_form_check.php
+++ shop_form_check.php
@@ -90,7 +90,7 @@ else
 
 if($okflg==true)
 {
-	print '<form method="post" action="shop_form-done.php">';
+	print '<form method="post" action="shop_form_done.php">';
 	print '<input type="hidden" name="onamae" value="'.$onamae.'">';
 	print '<input type="hidden" name="email" value="'.$email.'">';
 	print '<input type="hidden" name="postal1" value="'.$postal1.'">';
